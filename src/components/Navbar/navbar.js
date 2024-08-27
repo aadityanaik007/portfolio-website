cae35a40631b5a9e34554714a8ff1b9b9fbd2f7a
aaditya.naik <aadityasnaik007@gmail.com>
2024-08-27 16:18:37 -0700

    Handburger menu added

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,28 +1,185 @@
-import React from 'react'
-import "./navbar.css"
-import logo from '../../assests/logo.png'
-import {Link} from "react-scroll"
-import contact from "../../assests/contact.png"
+import React, { useState } from 'react';
+import './navbar.css';
+import logo from '../../assests/logo.png';
+import contact from '../../assests/contact.png';
+import { Link } from 'react-scroll';
 
-const Navbar = ()=> {
+const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
-    <nav className='navbar'>
-      <img src={logo} alt="logo" className='logo' />
-      <div className="desktopMenu">
-        <Link className="desktopMenuListItem" activeClass='active' to="intro" spy={true} smooth={true} offset={-50} duration={500}>Home</Link>
-        <Link className="desktopMenuListItem" activeClass='active' to="skills" spy={true} smooth={true} offset={-50} duration={500}>About</Link>
-        <Link className="desktopMenuListItem" activeClass='active' to="Experience" spy={true} smooth={true} offset={-50} duration={500}>Experience</Link>
-        <Link className="desktopMenuListItem" activeClass='active' to="certifications" spy={true} smooth={true} offset={-50} duration={500}>Certification</Link>
-        <Link className="desktopMenuListItem" activeClass='active' to="Portfolio" spy={true} smooth={true} offset={-50} duration={500}>Portfolio</Link>
+    <nav className="navbar">
+      {/* Logo */}
+      <div className="navbar__logo">
+        <img src={logo} alt="Logo" />
       </div>
-      <button id="contactMeBtn" className="desktopMenuBtn" onClick={()=>{
-        document.getElementById('contact').scrollIntoView({behavior:'smooth'});
-      }}>
-        <img src={contact} alt="contact" className="desktopMenuImg"/>
-        Contact Me
-      </button>
-    </nav>
-  )
-}
 
-export default Navbar
+      {/* Desktop Menu */}
+      <div className="navbar__menu">
+        <Link
+          className="navbar__item"
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          Home
+        </Link>
+        <Link
+          className="navbar__item"
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          About
+        </Link>
+        <Link
+          className="navbar__item"
+          activeClass="active"
+          to="experience"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          Experience
+        </Link>
+        <Link
+          className="navbar__item"
+          activeClass="active"
+          to="certifications"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          Certification
+        </Link>
+        <Link
+          className="navbar__item"
+          activeClass="active"
+          to="portfolio"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          Portfolio
+        </Link>
+      </div>
+
+      {/* Contact Button */}
+      <div className="navbar__contact">
+        <button
+          className="contact__button"
+          onClick={() => {
+            document
+              .getElementById('contact')
+              .scrollIntoView({ behavior: 'smooth' });
+          }}
+        >
+          <img src={contact} alt="Contact" className="contact__icon" />
+          Contact Me
+        </button>
+      </div>
+
+      {/* Mobile Menu Icon */}
+      <div className="navbar__toggle" onClick={toggleMobileMenu}>
+        <span className={`bar ${isMobileMenuOpen ? 'bar1' : ''}`}></span>
+        <span className={`bar ${isMobileMenuOpen ? 'bar2' : ''}`}></span>
+        <span className={`bar ${isMobileMenuOpen ? 'bar3' : ''}`}></span>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`navbar__mobile-menu ${
+          isMobileMenuOpen ? 'navbar__mobile-menu--active' : ''
+        }`}
+      >
+        <Link
+          className="navbar__mobile-item"
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          onClick={toggleMobileMenu}
+        >
+          Home
+        </Link>
+        <Link
+          className="navbar__mobile-item"
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          onClick={toggleMobileMenu}
+        >
+          About
+        </Link>
+        <Link
+          className="navbar__mobile-item"
+          activeClass="active"
+          to="experience"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          onClick={toggleMobileMenu}
+        >
+          Experience
+        </Link>
+        <Link
+          className="navbar__mobile-item"
+          activeClass="active"
+          to="certifications"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          onClick={toggleMobileMenu}
+        >
+          Certification
+        </Link>
+        <Link
+          className="navbar__mobile-item"
+          activeClass="active"
+          to="portfolio"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+          onClick={toggleMobileMenu}
+        >
+          Portfolio
+        </Link>
+        <button
+          className="contact__button" /* Apply the same class here */
+          onClick={() => {
+            document
+              .getElementById('contact')
+              .scrollIntoView({ behavior: 'smooth' });
+            toggleMobileMenu();
+          }}
+        >
+          <img src={contact} alt="Contact" className="contact__icon" />
+          Contact Me
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
